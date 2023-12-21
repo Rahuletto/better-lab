379698d98f6ff4403fe92f7b91055a18a63beea2
Rahul Marban <rahulmarban@gmail.com>
2023-12-21 07:05:25 +0000

    login flow

---
+++ pages/api/getreg.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	// Get url query parameters
+	const { user } = req.query;
+
+	const json = {
+		info: {
+			USER_ID: user,
+			ROLE: "S",
+		},
+		KEY: "john",
+	};
+
+	const JSONdata = JSON.stringify(json);
+
+	fetch(
+		"https://dld.srmist.edu.in/ktretelab2023/elabserver/ict/student/home/registeredcourses",
+		{
+			method: "POST",
+			body: JSONdata,
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": "https://dld.srmist.edu.in",
+				Referer: "https://dld.srmist.edu.in/ktretelab2023/",
+				Origin: "https://dld.srmist.edu.in",
+				Host: "dld.srmist.edu.in",
+				"Sec-Fetch-Mode": "cors",
+				"Sec-Fetch-Site": "same-origin",
+			},
+		},
+	)
+		.then((dt) => dt.json())
+		.then((data) => {
+			res.status(200).json(data);
+		});
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,43 @@
-import styles from "./QuestionDisplay.module.css";
-const Login = () => {
-  return (
-    <main>
+import { useEffect, useState } from "react";
+import styles from "../styles/Login.module.css";
+import { useRouter } from "next/router";
 
-    </main>
-  )
-};
+export default function Login() {
+	const router = useRouter();
+	const [uid, setUid] = useState("");
+	const [error, setError] = useState(0);
 
-export default Login;
+	function save() {
+		if (uid.length != 12) return setError(1);
+        setError(2)
+		localStorage.setItem("userid", uid);
+		router.push("/");
+	}
+
+    useEffect(() => {
+        const no = localStorage.getItem("userid");
+        if(no) router.push('/')
+    }, [])
+	return (
+		<main>
+			<div className={styles.container}>
+				<h1>Better-Lab</h1>
+				<div className={styles.login}>
+					<input
+						value={uid}
+                        maxLength={12}
+                        pattern="[0-9]{12}"
+                        minLength={12}
+                        style={error == 1 ? {border: "1px solid var(--red)", background: "#D133330e"} : error == 2 ?  {border: "1px solid var(--green)", background: "#65d1330e"} : {border: "1px solid transparent"}}
+						onChange={(e) => {
+							setError(0);
+							setUid(e.target.value);
+						}}
+						placeholder="User ID"
+					/>
+					<button onClick={save}>Login</button>
+				</div>
+			</div>
+		</main>
+	);
+}
