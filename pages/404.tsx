9047b71f28103acdec2645182db4a2f87e810a44
Rahul Marban <rahulmarban@gmail.com>
2023-12-22 15:49:29 +0000

    error messages

---
+++ pages/404.tsx
@@ -0,0 +1,32 @@
+import { TbError404 } from 'react-icons/tb';
+import styles from '@/styles/Error.module.css';
+import { useRouter } from 'next/router';
+
+
+export default function error() {
+  const router = useRouter()
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1 style={{ fontSize: '98px', color: "var(--yellow)" }}>
+          <TbError404 />
+        </h1>
+        <h3>uh oh.</h3>
+        <p>You are not supposed to be here comrade</p>
+        <button
+              className={styles.closebutton}
+              type="button"
+              style={{
+                borderWidth: 1.8,
+              }}
+              onClick={() =>
+                router.push('/')
+              }
+              title="Settings">
+              Return to Home
+            </button>
+      </div>
+    </main>
+  );
+}
---
+++ pages/500.tsx
@@ -0,0 +1,29 @@
+import { FaQuestion } from 'react-icons/fa';
+import styles from '@/styles/Error.module.css';
+import { useRouter } from 'next/router';
+
+export default function error() {
+  const router = useRouter();
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1 style={{ fontSize: '98px', color: "var(--red)" }}>
+          <FaQuestion />
+        </h1>
+        <h3>wait wha-</h3>
+        <p>Sounds like a broken socket. Don{"'"}t get zapped.<br />Error 500</p>
+        <button
+          className={styles.closebutton}
+          type="button"
+          style={{
+            borderWidth: 1.8,
+          }}
+          onClick={() => router.push('/')}
+          title="Settings">
+          Return to Home
+        </button>
+      </div>
+    </main>
+  );
+}
