bf5e91c80705995d04cf650a9523f559dbab6c74
Rahul Marban <rahulmarban@gmail.com>
2023-12-22 12:08:12 +0000

    making event for touch

--- components/QuestionDisplay/QuestionDisplay.tsx
+++ components/QuestionDisplay/QuestionDisplay.tsx
@@ -37,16 +37,17 @@ import { RiEmotionHappyFill } from "react-icons/ri";
 
 export function dialogHandler(e: MouseEvent | any) {
 	if (e.target?.tagName !== "DIALOG")
-		//This prevents issues with forms
 		return;
 
 	const rect = e.target.getBoundingClientRect();
 
+	const touch = (e as TouchEvent ? e.changedTouches[0] : e)
+
 	const clickedInDialog =
-		rect.top <= e.clientY &&
-		e.clientY <= rect.top + rect.height &&
-		rect.left <= e.clientX &&
-		e.clientX <= rect.left + rect.width;
+		rect.top <= touch.clientY &&
+		touch.clientY <= rect.top + rect.height &&
+		rect.left <= touch.clientX &&
+		touch.clientX <= rect.left + rect.width;
 
 	if (clickedInDialog === false) e.target.close();
 }
